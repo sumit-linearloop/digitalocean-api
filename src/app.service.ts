62f3e6d4c43e022375e5ba5a495bf425c01c359c
sumit-linearloop <sumit@linearloop.io>
2024-10-18 12:00:51 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,13 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return (
+      'Hello World! + ' +
+      'NODE_ENV ' +
+      process.env.NODE_ENV +
+      ' + ' +
+      'PORT ' +
+      process.env.PORT
+    );
   }
 }
