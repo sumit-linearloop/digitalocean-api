cf5311407f9c7c942944af58c661eecba97ab1f9
sumit-linearloop <sumit@linearloop.io>
2024-10-18 12:03:32 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(1500);
+  const PORT = process.env.PORT || 3000;
+  const ENV = process.env.NODE_ENV || 'development';
+  await app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} +  le laie lee`);
+    console.log(`Environment: ${ENV}`);
+  });
 }
 bootstrap();
