792896a2bfe870c1f17abbbf6626aec12d08da9a
sumit-linearloop <sumit@linearloop.io>
2024-10-23 11:31:32 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # Configuration variables
 GIT_REPO="git@github.com:sumit-linearloop/digitalocean-api.git"
 BRANCH_NAME="master"
-WORK_DIR="~/master"  # Use home directory for permissions
+WORK_DIR="/var/www/app"  # Use home directory for permissions
 
 # Function to check SSH connection
 check_ssh_connection() {
